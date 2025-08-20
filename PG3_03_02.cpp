5ac5a3591de0e2b5b4dea0cccbdf4b87cc668f0b
iwanaga1 <k024g1001@g.neec.ac.jp>
2025-08-20 12:40:57 +0900

    プロジェクト ファイルを追加します。

---
+++ PG3_03_02.cpp
@@ -0,0 +1,77 @@
+﻿// PG3_03_02.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+	vector<string> IDNum = {
+		"k022g0108@g.neec.ac.jp","k022g0045@g.neec.ac.jp","k022g0007@g.neec.ac.jp",
+		"k022g0015@g.neec.ac.jp","k022g0028@g.neec.ac.jp","k022g0049@g.neec.ac.jp",
+		"k022g0003@g.neec.ac.jp","k022g0044@g.neec.ac.jp","k022g0072@g.neec.ac.jp",
+		"k022g0055@g.neec.ac.jp","k021g1139@g.neec.ac.jp","k021g1485@g.neec.ac.jp",
+		"k021g1162@g.neec.ac.jp","k021g1278@g.neec.ac.jp","k020g1229@g.neec.ac.jp",
+		"k020g1202@g.neec.ac.jp","k020g1215@g.neec.ac.jp","k020g1416@g.neec.ac.jp",
+		"k020g1245@g.neec.ac.jp","k020g1506@g.neec.ac.jp","k020g1401@g.neec.ac.jp",
+		"k022g0089@g.neec.ac.jp","k022g0107@g.neec.ac.jp","k022g0135@g.neec.ac.jp",
+		"k022g0020@g.neec.ac.jp","k022g0046@g.neec.ac.jp","k022g0126@g.neec.ac.jp",
+		"k022g0068@g.neec.ac.jp","k022g1060@g.neec.ac.jp","k022g0019@g.neec.ac.jp",
+		"k022g0013@g.neec.ac.jp","k022g0024@g.neec.ac.jp","k022g0134@g.neec.ac.jp",
+		"k022g0123@g.neec.ac.jp","k022g0030@g.neec.ac.jp","k022g0053@g.neec.ac.jp",
+		"k022g0023@g.neec.ac.jp","k022g0041@g.neec.ac.jp","k022g0063@g.neec.ac.jp",
+		"k022g0059@g.neec.ac.jp","k022g0078@g.neec.ac.jp","k022g0011@g.neec.ac.jp",
+		"k022g0040@g.neec.ac.jp","k022g0088@g.neec.ac.jp","k022g0099@g.neec.ac.jp",
+		"k022g0060@g.neec.ac.jp","k022g0032@g.neec.ac.jp","k022g0087@g.neec.ac.jp",
+		"k022g0016@g.neec.ac.jp","k022g0116@g.neec.ac.jp","k022g0056@g.neec.ac.jp",
+		"k022g0009@g.neec.ac.jp","k022g0034@g.neec.ac.jp","k022g0062@g.neec.ac.jp",
+		"k022g0102@g.neec.ac.jp","k022g0113@g.neec.ac.jp","k022g0064@g.neec.ac.jp",
+		"k022g0080@g.neec.ac.jp","k022g0114@g.neec.ac.jp","k022g0025@g.neec.ac.jp",
+		"k022g0095@g.neec.ac.jp","k022g0121@g.neec.ac.jp","k022g0021@g.neec.ac.jp",
+		"k022g0104@g.neec.ac.jp","k022g0101@g.neec.ac.jp","k022g0130@g.neec.ac.jp",
+		"k022g0036@g.neec.ac.jp","k022g0004@g.neec.ac.jp","k022g0093@g.neec.ac.jp",
+		"k022g0038@g.neec.ac.jp","k022g0069@g.neec.ac.jp","k022g0029@g.neec.ac.jp",
+		"k022g0010@g.neec.ac.jp","k022g0083@g.neec.ac.jp","k022g0086@g.neec.ac.jp",
+		"k022g0129@g.neec.ac.jp","k022g0048@g.neec.ac.jp","k022g0027@g.neec.ac.jp",
+		"k022g0103@g.neec.ac.jp","k022g0117@g.neec.ac.jp","k022g0085@g.neec.ac.jp",
+		"k022g0052@g.neec.ac.jp","k022g0073@g.neec.ac.jp","k022g0065@g.neec.ac.jp",
+		"k022g0091@g.neec.ac.jp","k022g0136@g.neec.ac.jp","k022g0079@g.neec.ac.jp",
+		"k022g0017@g.neec.ac.jp","k022g0097@g.neec.ac.jp","k022g0077@g.neec.ac.jp",
+		"k022g0050@g.neec.ac.jp","k022g0033@g.neec.ac.jp","k022g0039@g.neec.ac.jp",
+		"k022g0118@g.neec.ac.jp","k022g0112@g.neec.ac.jp","k022g0120@g.neec.ac.jp",
+		"k022g0127@g.neec.ac.jp","k021g1464@g.neec.ac.jp","k022g0137@g.neec.ac.jp",
+		"k022g0066@g.neec.ac.jp","k022g0051@g.neec.ac.jp","k022g0111@g.neec.ac.jp",
+		"k022g0094@g.neec.ac.jp","k022g0128@g.neec.ac.jp","k022g0109@g.neec.ac.jp",
+		"k022g0124@g.neec.ac.jp","k022g0084@g.neec.ac.jp","k022g0082@g.neec.ac.jp",
+		"k022g0122@g.neec.ac.jp","k022g0061@g.neec.ac.jp","k022g0031@g.neec.ac.jp",
+		"k022g0076@g.neec.ac.jp","k022g0002@g.neec.ac.jp","k022g0090@g.neec.ac.jp",
+		"k022g0074@g.neec.ac.jp","k022g0012@g.neec.ac.jp","k022g0001@g.neec.ac.jp"
+	};
+
+	sort(IDNum.begin(),IDNum.end());
+
+	cout << "学籍番号" << endl;
+
+	for (int i = 0; i < IDNum.size(); i++)
+		cout << IDNum[i] << endl;
+
+	return 0;
+}
+
+   
+
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
